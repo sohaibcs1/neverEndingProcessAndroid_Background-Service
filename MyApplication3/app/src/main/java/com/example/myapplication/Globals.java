62e0689c8e5b8ca592a876b2417752f5006ec3f4
Muhammad Sohaib <43402659+sohaibcs1@users.noreply.github.com>
2022-01-10 21:26:58 +0500

    ksl

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/Globals.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2019. This code has been developed by Fabio Ciravegna, The University of Sheffield. All rights reserved. No part of this code can be used without the explicit written permission by the author
+ */
+
+package com.example.myapplication;
+
+public class Globals {
+    public static final String RESTART_INTENT = "com.example.restarter";
+}
---
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import com.example.myapplication.restarter.RestartServiceBroadcastReceiver;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            RestartServiceBroadcastReceiver.scheduleJob(getApplicationContext());
+        } else {
+            ProcessMainClass bck = new ProcessMainClass();
+            bck.launchService(getApplicationContext());
+        }
+    }
+}
